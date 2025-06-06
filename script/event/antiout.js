16608cb8ae8091a2071384c5cf380bccc8d5531b
vernesg <veaxdev36@gmail.com>
2025-06-06 14:28:00 +0800

    Create event/antiout.js

---
+++ script/event/antiout.js
@@ -0,0 +1,23 @@
+module.exports.config = {
+  name: "antiout",
+  version: "1.0.0"
+};
+module.exports.handleEvent = async ({
+  event,
+  api
+}) => {
+  if (event.logMessageData?.leftParticipantFbId === api.getCurrentUserID()) return;
+  if (event.logMessageData?.leftParticipantFbId) {
+    const info = await api.getUserInfo(event.logMessageData?.leftParticipantFbId);
+    const {
+      name
+    } = info[event.logMessageData?.leftParticipantFbId];
+    api.addUserToGroup(event.logMessageData?.leftParticipantFbId, event.threadID, (error) => {
+      if (error) {
+        api.sendMessage(`di ko ma add si ${name} iwan ko kong bakit`, event.threadID);
+      } else {
+        api.sendMessage(`boss ${name} bawal kapo mag leave tanginamo paalam ka muna kay vern`, event.threadID);
+      }
+    });
+  }
+};
