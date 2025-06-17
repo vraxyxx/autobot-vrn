5f639988c0825216e970404abe5daaf6709b9c3e
vernesg <veaxdev36@gmail.com>
2025-06-17 09:06:32 +0800

    Update autoreact.js

--- script/event/autoreact.js
+++ script/event/autoreact.js
@@ -1,39 +1,33 @@
- module.exports = {
-  config: {
-    name: "autoreact",
-    author: "Vern",
-    version: "1.0",
-    countDown: 5,
-    role: 0,
-    shortDescription: "Auto React",
-    longDescription: " ",
-    category: "System",
-  },
-  onStart: async function () {
-    // Add initialization logic here if needed
-  },
-  onChat: async function ({ event, api }) {
-    const message = event.body.toLowerCase();
+module.exports.config = {
+  name: "autoreact",
+  type: "event",
+  eventType: ["message"],
+  version: "1.0",
+  credits: "Vern", // DO NOT CHANGE
+};
 
-    const reactionsMap = {
-      "😂": ["haha", "lol", "funny", "hahah", "hahaha", "masaya", "happy", "🤣", "natomba", "tumomba", "tomomba", "tumumba", "tomumba", "side eye", "awooop jumpscare", "so masaya ka?", "sana all"],
-      "😭": ["cry", "sad", "crying", "bakit ka malungkot?", "bakit ka malongkot?", "hindi na", "sad ka", "walang ulam"],
-      "🥰": ["love", "mahal", "crush"],
-      "🎮": ["laro", "laru", "game", "mc", "minecraft", "ml", "mlbb", "mobile legends", "mobile legends bang bang", "cod", "call of duty"]
-      // Add more reactions and associated keywords as needed
-    };
+module.exports.run = async function ({ api, event }) {
+  if (!event.body) return;
+  const message = event.body.toLowerCase();
 
-    console.log("Message:", message);
+  const reactionsMap = {
+    "😂": [
+      "haha", "lol", "funny", "hahah", "hahaha", "masaya", "happy", "🤣",
+      "natomba", "tumomba", "tomomba", "tumumba", "tomumba",
+      "side eye", "awooop jumpscare", "so masaya ka?", "sana all"
+    ],
+    "😭": [
+      "cry", "sad", "crying", "bakit ka malungkot?", "bakit ka malongkot?",
+      "hindi na", "sad ka", "walang ulam"
+    ],
+    "🥰": ["love", "mahal", "crush"],
+    "🎮": ["laro", "laru", "game", "mc", "minecraft", "ml", "mlbb", "mobile legends", "mobile legends bang bang", "cod", "call of duty"]
+  };
 
-    for (const [reaction, keywords] of Object.entries(reactionsMap)) {
-      console.log("Reaction:", reaction);
-      console.log("Keywords:", keywords);
-
-      if (keywords.some((word) => message.includes(word))) {
-        console.log("Reacting with:", reaction);
-        api.setMessageReaction(reaction, event.messageID, event.threadID, api);
-        break; // Stop checking once a reaction is set
-      }
+  for (const [reaction, keywords] of Object.entries(reactionsMap)) {
+    if (keywords.some(word => message.includes(word))) {
+      api.setMessageReaction(reaction, event.messageID, event.threadID, api);
+      break;
     }
-  },
+  }
 };
