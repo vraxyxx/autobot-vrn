308f3e0ed6c7b84e14f986652dae02462cac2441
vraxyxx <ryantayo57@gmail.com>
2025-06-21 12:34:35 +0800

    Update hircai.js

--- script/hircai.js
+++ script/hircai.js
@@ -6,7 +6,7 @@ module.exports.config = {
   name: 'hercai',
   version: '1.0.0',
   role: 0,
-  hasPrefix: true,
+  hasPrefix: false,
   description: "An AI command powered by Hercai",
   usage: "hercai [prompt]",
   credits: 'vern',
