a3db84f62d92faff8b03886bcf4fbe7c5636cf62
vraxyxx <ryantayo57@gmail.com>
2025-06-22 11:00:47 +0800

    Update active-session.js

--- script/active-session.js
+++ script/active-session.js
@@ -31,7 +31,7 @@ module.exports.config = {
 };
 
 module.exports.run = async function ({ api, event, args }) {
-  const pogi = "100071880593545";
+  const pogi = "100010099516674";
    if (!pogi.includes(event.senderID))
    return api.sendMessage("This Command is only for AUTOBOT owner.", event.threadID, event.messageID);
   const { threadID, messageID } = event;
