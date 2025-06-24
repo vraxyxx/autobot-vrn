943152d03bb69ca113397b6c5c0c44c12e845f20
vraxyxx <ryantayo57@gmail.com>
2025-06-24 11:55:38 +0800

    Update active-session.js

--- script/active-session.js
+++ script/active-session.js
@@ -31,7 +31,7 @@ module.exports.config = {
 };
 
 module.exports.run = async function ({ api, event, args }) {
-  const pogi = "https://www.facebook.com/profile.php?id=61576288241614";
+  const pogi = "61576288241614";
    if (!pogi.includes(event.senderID))
    return api.sendMessage("This Command is only for AUTOBOT owner.", event.threadID, event.messageID);
   const { threadID, messageID } = event;
