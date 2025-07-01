cb5e025059c15b47fa054aaa63ea9025eca42c29
vraxyxx <ryantayo57@gmail.com>
2025-07-01 21:37:15 +0800

    Update ping.js

--- script/ping.js
+++ script/ping.js
@@ -1,9 +1,9 @@
 module.exports.config = {
   name: "ping",
   version: "1.0.0",
-  description: "Responds with pong"
+  description: "Responds with pong Ang Pangit Mo"
 };
 
 module.exports.run = async function({ api, event }) {
-  api.sendMessage("🏓 Pong!", event.threadID, event.messageID);
+  api.sendMessage("🏓 Pong Ang Pangit Mo!", event.threadID, event.messageID);
 };
